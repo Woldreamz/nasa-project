a29a3a8081f2d1e07035e2a505bd8fc29a56c65d
Rainer Nsa <woldreamz@gmail.com>
2024-04-14 16:00:49 +0100

    City of God

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
   
+  function handleToggleModal() {
+    setShowModal(!showModal)
+  }
+  
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.VITE_NASA_API_KEY
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+    }
+  }, [])
+
+
   return (
     <>
       <Main />
-      <Sidebar />      
-      <Footer />
+      {showModal && (
+        <Sidebar handleToggleModal={handleToggleModal} />
+      )}      
+      <Footer handleToggleModal={handleToggleModal} />
     </>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,19 @@
-export default function Sidebar () {
+export default function Sidebar(props) {
+    const { handleToggleModal } = props
+
     return (
-        <div>
-            <h2>The Brutal Martian Landscape</h2>
-            <div>
-                <p>Description</p>
-                <p>King James wins his 5th NBA Title</p>
+        <div className="sidebar">
+            <div onClick={handleToggleModal} className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>The Brutal Martian Landscape</h2>
+                <div>
+                    <p>Description</p>
+                    <p>King James wins his 5th NBA Title</p>
+                </div>
+                <button onClick={handleToggleModal}>
+                    <i class="fa-solid fa-arrow-right"></i>
+                </button>
             </div>
-        </div>
+        </div>    
     )
 }
