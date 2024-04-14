606486020a25d1310c8d1bf70069c0c721626bef
Rainer Nsa <woldreamz@gmail.com>
2024-04-14 14:04:28 +0100

    City of God

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 export default function Sidebar () {
     return (
-        <>
-        
-        </>
+        <div>
+            <h2>The Brutal Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+                <p>King James wins his 5th NBA Title</p>
+            </div>
+        </div>
     )
 }
